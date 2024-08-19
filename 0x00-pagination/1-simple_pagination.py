e9cc30ee6478757ab73c205aaa75966a43815862
Lumasia <lumasiastancelaus@gmail.com>
2024-08-19 21:26:29 +0300

    Added a shebang

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+"""Simple pagination"""
 import csv
 import math
 from typing import List
@@ -8,6 +10,7 @@ def index_range(page: int, page_size: int) -> tuple:
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return start_index, end_index
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
