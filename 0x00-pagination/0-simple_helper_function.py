7befe5ef25936d070f27c7158d7b7e4fe245646b
Lumasia <lumasiastancelaus@gmail.com>
2024-08-19 10:22:03 +0300

    Simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Simple helper function"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Returns a tuple containing start and end index"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
