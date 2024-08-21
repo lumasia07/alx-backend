162cf41166706360f21501f90fb63ea97a602de5
Lumasia <lumasiastancelaus@gmail.com>
2024-08-21 23:04:01 +0300

    lifo cache

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""LIFO caching"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    Returns a cached dictionary
+    """
+    def __init__(self):
+        super().__init__()
+        self.keys_order = []
+
+    def put(self, key, item):
+        """Adds key-value pair to dictionary"""
+        if key is None or item is None:
+            return
+
+        if key not in self.cache_data:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                newest_key = self.keys_order.pop(-1)
+                del self.cache_data[newest_key]
+                print(f"DISCARD: {newest_key}")
+
+        self.cache_data[key] = item
+        if key not in self.keys_order:
+            self.keys_order.append(key)
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
