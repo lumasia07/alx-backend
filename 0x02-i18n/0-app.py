8d4a7f5987a46e26c083e12f9d1fa3b9860bb83e
Lumasia <lumasiastancelaus@gmail.com>
2024-08-27 09:28:59 +0300

    Flask App

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Flask App"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+
+
+def index():
+    """Index template"""
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
