5af7bccd86d80ad82be92aaa71d789fb786c4d2d
Lumasia <lumasiastancelaus@gmail.com>
2024-08-27 20:30:58 +0300

    added doccstrings

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Basic Flask Babel setup"""
 from flask import Flask, render_template
 from flask_babel import Babel
 
@@ -7,6 +8,7 @@ app = Flask(__name__)
 
 
 class Config:
+    """Basic config"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -19,6 +21,7 @@ babel = Babel(app)
 
 @app.route('/')
 def index():
+    """Index page"""
     return render_template('1-index.html')
 
 
