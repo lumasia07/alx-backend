fc65c2d4941ee3f809f90a9c342d186c0a30557a
Lumasia <lumasiastancelaus@gmail.com>
2024-08-27 20:15:25 +0300

    fixed pyocodestyle issues

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,11 +6,10 @@ app = Flask(__name__)
 
 
 @app.route('/')
-
-
 def index():
     """Index template"""
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
