430bbb3ac0dd04b125d951464d29b271fcca05c2
Lumasia <lumasiastancelaus@gmail.com>
2024-08-30 09:01:38 +0300

    mocked user login

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""Mock user login"""
+from flask import Flask, request, g, render_template
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'fr']
+
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    if g.user and g.user.get('locale'):
+        return g.user['locale']
+    locale = request.args.get('locale')
+    if locale and locale in app.config['BABEL_SUPPORTED_LOCALES']:
+        return locale
+    return request.accept_languages.best_match(app.config['BABEL_SUPPORTED_LOCALES'])
+
+
+def get_user():
+    """Retrieve a user based on the login_as param"""
+    try:
+        user_id = int(request.args.get('login_as'))
+        return users.get(user_id)
+    except (TypeError, ValueError):
+        return None
+
+
+@app.before_request
+def before_request():
+    """Set the user in the global g object before each request"""
+    g.user = get_user()
+
+
+@app.route('/')
+def index():
+    """Render template"""
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
