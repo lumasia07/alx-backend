ee82171ad5eefe218cb51bcabb653d6a65266e86
Lumasia <lumasiastancelaus@gmail.com>
2024-09-25 13:21:25 +0300

    Created my first job with kue

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,23 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const jobData = {
+  phoneNumber: '1234567890',
+  message: 'This is a notification message'
+};
+
+const job = queue.create('push_notification_code', jobData)
+  .save((err) => {
+    if (!err) {
+      console.log(`Notification job created: ${job.id}`);
+    } else {
+      console.log('Failed to create job', err);
+    }
+  });
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+}).on('failed', () => {
+  console.log('Notification job failed');
+});
