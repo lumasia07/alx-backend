81919b8e2b6f3744b2ec92495d96b94be11170b6
Lumasia <lumasiastancelaus@gmail.com>
2024-09-25 18:16:49 +0300

    8 job

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,32 @@
+import kue from 'kue';
+
+function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+
+  jobs.forEach((jobData) => {
+    const job = queue.create('push_notification_code_3', jobData);
+
+    job.on('enqueue', () => {
+      console.log(`Notification job created: ${job.id}`);
+    });
+
+    job.on('complete', () => {
+      console.log(`Notification job ${job.id} completed`);
+    });
+
+    job.on('failed', (err) => {
+      console.log(`Notification job ${job.id} failed: ${err}`);
+    });
+
+    job.on('progress', (progress) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+
+    job.save();
+  });
+}
+
+// Export the function for external usage
+export default createPushNotificationsJobs;
